e8ea6836df62fec2f6bdc1bdbb9d2d9f57381a13
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-25 04:01:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,5 +53,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+if (cardNumber == undefined) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    let totalPrice = total();
+    setCart([]);
+    return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`;
+  }
 }
