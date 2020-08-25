f7ba2525db7f8313a0c60b310a606bc4575f60b2
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-25 03:05:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ new object.assigne(itemName: name of the item, itemPrice: price of the item)
+ 
 }
 
 function viewCart() {
