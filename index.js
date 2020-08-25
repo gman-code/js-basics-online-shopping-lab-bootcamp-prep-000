ff695b4b16c0c25abfec1a85118383f666134fa2
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-25 03:58:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ function total() {
 }
 
 function removeFromCart(item) {
+  
    for (var i = 0; i < cart.length; i++){
     if (item === cart[i].itemName) {
       cart.splice(i, 1); //removes 1 item at index i
