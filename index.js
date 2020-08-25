76ff27bbd581417de367ecb0fff00d81a146885e
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-25 03:53:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,22 +18,9 @@ function addToCart(item) {
 
 
 function viewCart() {
-  if (getCart().length === 0) {
-    return "Your shopping cart is empty.";
-  } else {
-    let out = "In your cart, you have ";
-    for (let i=0; i < getCart().length; i++) {
-      if (getCart().length > 1 && i === getCart().length-1) {
-        out = out + "and ";
-      }
-      let val = getCart()[i].itemName + " at $" + getCart()[i].itemPrice;
-      out = out + val;
-      if (getCart().length > 1 && i != getCart().length-1) {
-        out = out + ", ";
-      }
-    }
-    return out + ".";
+  
 }
+  
 
 function total() {
   // write your code here
