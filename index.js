7926b7bcce23854763e5820b9c36a6e5e371c46a
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-25 03:45:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,20 +18,7 @@ function addToCart(item) {
 
 
 function viewCart() {
- var itemsandprices = [];
-  var intro = 'In your cart, you have';
-  for (var i = 0; i < cart.length; i++){
-    var itemx = Object.keys(cart[i]);
-    var pricex = cart[i][itemx];
-        itemsandprices.push(itemx +' at $'+ pricex)  }
-    var dositems = itemsandprices.join(' and ');
-    var beginning = itemsandprices.slice(0, -1).join(', ');
-    var end = itemsandprices.slice(-1);
 
-  if (cart.length === 0){console.log('Your shopping cart is empty.')}
-  else if (cart.length === 1){console.log(`${intro} ${itemsandprices}.`)}
-  else if (cart.length === 2){console.log(`${intro} ${dositems}.`)}
-  else if (cart.length > 2){return(`${intro} ${beginning}, and ${end}.`)}
 }
 function total() {
   // write your code here
